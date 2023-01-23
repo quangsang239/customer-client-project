df84ffab6e9f855755dd488fee38d1ae79b2d1b2
quangsang239 <quangsang239@gmail.com>
2023-01-23 18:28:41 +0700

    base source

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Roboto } from "@next/font/google";
+import { SWRConfig } from "swr";
+import axiosClient from "@/api-client/axios-client";
+import { ToastContainer } from "react-toastify";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
@@ -10,8 +13,16 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <SWRConfig
+      value={{
+        fetcher: async (url) => await axiosClient.get(url),
+        shouldRetryOnError: false,
+      }}
+    >
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </main>
+    </SWRConfig>
   );
 }
